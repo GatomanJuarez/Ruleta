3465c84eedb9fed501e6d4593f04a0c4c86b87be
Jesus Antonio JD <juarez_devora_ja@hotmail.com>
2017-05-12 07:13:15 -0500

    Add files via upload

---
+++ Version1/Ruleta/Ruleta/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Ruleta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.lbMostrar = new System.Windows.Forms.Label();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnAgregar.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(12, 66);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(92, 33);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Agregar.";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(12, 118);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(92, 33);
+            this.btnEliminar.TabIndex = 1;
+            this.btnEliminar.Text = "Eliminar.";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnGenerar.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerar.Location = new System.Drawing.Point(12, 12);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(92, 33);
+            this.btnGenerar.TabIndex = 2;
+            this.btnGenerar.Text = "Generar.";
+            this.btnGenerar.UseVisualStyleBackColor = false;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // lbMostrar
+            // 
+            this.lbMostrar.AutoSize = true;
+            this.lbMostrar.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMostrar.ForeColor = System.Drawing.Color.Black;
+            this.lbMostrar.Location = new System.Drawing.Point(141, 12);
+            this.lbMostrar.Name = "lbMostrar";
+            this.lbMostrar.Size = new System.Drawing.Size(75, 19);
+            this.lbMostrar.TabIndex = 3;
+            this.lbMostrar.Text = "Resultado:";
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.Location = new System.Drawing.Point(12, 166);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(92, 33);
+            this.btnMostrar.TabIndex = 4;
+            this.btnMostrar.Text = "Mostrar.";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(496, 219);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.lbMostrar);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ruleta";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label lbMostrar;
+        private System.Windows.Forms.Button btnMostrar;
+    }
+}
+
---
+++ Version1/Ruleta/Ruleta/Form1.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InputKey;
+
+//Powered By GatomanJuarez.
+
+namespace Ruleta
+{
+    public partial class Form1 : Form
+    {
+        //Declaramos nuestras variables.
+        //Lista para almacenar la informacion del txt al eliminar algun nombre.
+        private List<string> personas = new List<string>();
+        //Variable para almacenar el nombre nuevo, el nombre a eliminar y la ruta.
+        private String nombre, eliminar, ruta = @"Datos.txt";
+        //Variable para escribir en el txt.
+        private System.IO.StreamWriter escritor;
+        //Variable para leer el txt.
+        private System.IO.StreamReader leerArchivo;
+        //Variable para verificar que los datos introducidos no sean numeros y/o caracteres.
+        private bool verificarN, verificarC;
+        //Numero random.
+        private Random random = new Random();
+        private int auxiliar;
+
+        //Metodos getters y setters.
+        public void setpersonas(List<string> personas)
+        {
+            this.personas = personas;
+        }
+
+        public void setnombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public void seteliminar(string eliminar)
+        {
+            this.eliminar = eliminar;
+        }
+
+        public void setauxiliar(int auxiliar)
+        {
+            this.auxiliar = auxiliar;
+        }
+
+        public void setrandom(Random random)
+        {
+            this.random = random;
+        }
+
+        public void setruta(string ruta)
+        {
+            this.ruta = ruta;
+        }
+
+        public void setleerArcivo(System.IO.StreamReader leerArchivo)
+        {
+            this.leerArchivo = leerArchivo;
+        }
+
+        public void setescritor(System.IO.StreamWriter escritor)
+        {
+            this.escritor = escritor;
+        }
+
+        public void setverificarN(bool verificarN)
+        {
+            this.verificarN = verificarN;
+        }
+
+        public void setverificarC(bool verificarC)
+        {
+            this.verificarC = verificarC;
+        }
+
+        public bool getverificarN(bool verificarN)
+        {
+            return verificarN;
+        }
+
+        public int getauxiliar(int auxiliar)
+        {
+            return auxiliar;
+        }
+
+        public bool getverificarC(bool verificarC)
+        {
+            return verificarC;
+        }
+
+        public Random getrandom(Random random)
+        {
+            return random;
+        }
+
+        public List<string> getpersonas(List<string> personas)
+        {
+            return personas;
+        }
+
+        public string getnombre(string nombre)
+        {
+            return nombre;
+        }
+
+        public string geteliminar(string eliminar)
+        {
+            return eliminar;
+        }
+
+        public string getruta(string ruta)
+        {
+            return ruta;
+        }
+
+        public System.IO.StreamWriter getescritor(System.IO.StreamWriter escritor)
+        {
+            return escritor;
+        }
+
+        public System.IO.StringReader getleerArchivo(System.IO.StringReader leerArchivo)
+        {
+            return leerArchivo;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            if (System.IO.File.Exists(ruta))
+            {
+                //Si existe el archivo.
+                //Mostramos el contenido del txt.
+                mostrar();
+            }
+            else
+            {
+                //Limpiamos nuestro label.
+                lbMostrar.Text = "";
+                //Creamos el archivo (en caso de que no exista).
+                crearArchivo();
+            }
+        }
+
+        //Verificamos si el contenido contiene algun numero.
+        public bool contieneNumero(string Expression)
+        {
+            bool isNum;
+            string[] caracteres = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            for (byte i = 0; i < caracteres.Length; i++)
+            {
+                if (Expression.Contains(caracteres[i]))
+                {
+                    isNum = true;
+                    return isNum;
+                }
+            }
+            isNum = false;
+            return isNum;
+        }
+
+        //Verificamos si el contenido contiene algun caracter.
+        public bool contieneCaracter(string Expression)
+        {
+            bool isChar;
+            Char[] caracteres = {'*', '/', '-', '+', '.', '|', '°', '¬', '!', '"', '#', '$', '%', '&', '(', ')', '=', '?','¡', '´', '¨', '*',
+                                 '~', '{','[' ,'^', ']', '}','`', ',','.',';','-','_' };
+            for (byte i = 0; i < caracteres.Length; i++)
+            {
+                if (Expression.Contains(caracteres[i]))
+                {
+                    isChar = true;
+                    return isChar;
+                }
+            }
+            isChar = false;
+            return isChar;
+        }
+
+        private void agregarPersonas()
+        {
+            //Try para capturar excepciones.
+            try
+            {
+                //Pedimos y almacenamos el nuevo nombre.
+                nombre = InputDialog.mostrar("Introduzca el nuevo nombre.");
+                //Verificamos que no contenga numeros.
+                verificarN = contieneNumero(nombre);
+                verificarC = contieneCaracter(nombre);
+                //En caso de contener alguno no lo guardamos.
+                if (verificarN || verificarC)
+                {
+                    //Si contiene algun numero o caracter mostramos el mensaje.
+                    MessageBox.Show("Introduca solo texto.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    //En caso de que no lo sea usamos nuestro objeto para escribir el nombre.
+                    using (escritor = System.IO.File.AppendText(ruta))
+                    {
+                        //Escribimos en nuestro txt el nombre.
+                        escritor.WriteLine(nombre);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                //Mostramos algun error en caso de que se tenga.
+                MessageBox.Show("Introduca solo texto.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void crearArchivo()
+        {
+            //Usamos nuestro objeto para crear el archivo.
+            using (escritor = new System.IO.StreamWriter(ruta))
+            {
+                //Escribimos un pequeño titulo.
+                escritor.WriteLine("Almacenamiento de nombres:");
+            }
+        }
+
+        private void mostrar()
+        {
+            try
+            {
+                //Declaramos una variable auxiliar.
+                string linea;
+                //Limpiamos nuestro label.
+                lbMostrar.Text = "";
+                //Usamos nuestro objeto para leer el txt.
+                using (leerArchivo = new System.IO.StreamReader(ruta))
+                {
+                    //Usamos el while hasta que no encentre mas lienas.
+                    while (leerArchivo.Peek() > -1)
+                    {
+                        //Todo lo escrito en las lineas las guardamos en nuestra variable.
+                        linea = leerArchivo.ReadLine();
+                        //Comprovamos que la variable tenga algun valor.
+                        if (!String.IsNullOrEmpty(linea))
+                        {
+                            //Lo imprimimos en nuestro label.
+                            lbMostrar.Text += linea + "\n";
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error", "Ha ocurrido un error.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void eliminarPersona()
+        {
+            try
+            {
+                //Guardamos el nombre a eliminar.
+                eliminar = InputDialog.mostrar("Introduzca el nombre \n completo para eliminar.", "Dato");
+                //Verificamos que no contenga numeros.
+                verificarN = contieneNumero(nombre);
+                verificarC = contieneCaracter(nombre);
+                //En caso de contener alguno no lo guardamos.
+                if (verificarN || verificarC)
+                {
+                    //Si contiene algun numero o caracter mostramos el mensaje.
+                    MessageBox.Show("Introduca solo texto.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    using (leerArchivo = new System.IO.StreamReader(ruta))
+                    {
+                        while (leerArchivo.Peek() > -1)
+                        {
+                            string linea = leerArchivo.ReadLine();
+                            if (!String.IsNullOrEmpty(linea))
+                            {
+                                //Lo que contiene el archivo que lo abrimos en modo lectura lo guardamos en una lista.
+                                personas.Add(linea);
+                            }
+                        }
+                    }
+                    //Preguntamos si en la lista esta nuestro nombre a eliminar.
+                    if (personas.Contains(eliminar))
+                    {
+                        //Y si esta lo eliminamos.
+                        personas.Remove(eliminar);
+                    }
+                    else
+                    {
+                        //Mostramos un mensaje en caso de que no.
+                        MessageBox.Show("No se ha encontrado el \n nombre a eliminar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                    //Esta lisa la guardamos en nuestro txt.
+                    using (escritor = new System.IO.StreamWriter(ruta))
+                    {
+                        foreach (string contenido in personas)
+                        {
+                            escritor.WriteLine(contenido);
+                        }
+                    }
+
+                    //Llamamos a nuestro metodo para observar cambios.
+                    mostrar();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error", "Introduca solo texto.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void generar()
+        {
+            try
+            {
+                personas.Clear();
+
+                using (leerArchivo = new System.IO.StreamReader(ruta))
+                {
+                    while (leerArchivo.Peek() > -1)
+                    {
+                        string linea = leerArchivo.ReadLine();
+                        if (!String.IsNullOrEmpty(linea))
+                        {
+                            //Lo que contiene el archivo que lo abrimos en modo lectura lo guardamos en una lista.
+                            personas.Add(linea);
+                        }
+                    }
+                }
+                //Generamos numero random.
+                auxiliar = random.Next(0, personas.Count);
+                //Lo imprimimos.
+                lbMostrar.Text = personas[auxiliar];
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error", "Introduca solo texto.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            lbMostrar.Text = "";
+            generar();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            escritor.Close();
+            leerArchivo.Close();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            agregarPersonas();
+            lbMostrar.Text = "";
+            mostrar();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            eliminarPersona();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            lbMostrar.Text = "";
+            mostrar();
+        }
+
+    }
+}
